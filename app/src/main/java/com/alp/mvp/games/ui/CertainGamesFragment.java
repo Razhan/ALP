d54a9c6d1fa6d53cb8ed75b0e7ed90f98c9fdaf2
DESKTOP-EFNOSL4\ranzh <ranzhang.su@outlook.com>
2017-01-13 16:27:38 +0800

    games overview layout done

--- app/src/main/java/com/alp/mvp/games/ui/CertainGamesFragment.java
+++ app/src/main/java/com/alp/mvp/games/ui/CertainGamesFragment.java
@@ -2,30 +2,39 @@ package com.alp.mvp.games.ui;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.alp.library.base.ui.BaseMVPFragment;
 import com.alp.mvp.ALPApplication;
 import com.alp.mvp.R;
-import com.alp.mvp.adapter.GameAdapter;
+import com.alp.mvp.adapter.GameListAdapter;
 import com.alp.mvp.di.components.DaggerGamesComponent;
 import com.alp.mvp.di.modules.ActivityModule;
 import com.alp.mvp.games.GamesContract;
 import com.alp.mvp.games.GamesPresenter;
+import com.alp.mvp.widgete.LazyRecycleView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class CertainGamesFragment extends BaseMVPFragment<GamesPresenter> implements GamesContract.View {
 
     @BindView(R.id.game_list)
-    RecyclerView gameList;
+    LazyRecycleView gameList;
+    @BindView(R.id.left_arrow)
+    ImageView leftArrow;
+    @BindView(R.id.right_arrow)
+    ImageView rightArrow;
 
-    private GameAdapter adapter;
+    private GameListAdapter adapter;
+    private int currentPos = 0;
 
     public static Fragment newInstance() {
         return new CertainGamesFragment();
@@ -54,17 +63,42 @@ public class CertainGamesFragment extends BaseMVPFragment<GamesPresenter> implem
 
     @Override
     public void initView(Bundle savedInstanceState) {
+//        initArrow();
         initList();
     }
 
+    private void initArrow() {
+        leftArrow.setColorFilter(ContextCompat.getColor(activity, R.color.grey));
+    }
+
     private void initList() {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            list.add("");
+        List<String> list1 = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            list1.add("");
         }
 
-        adapter = new GameAdapter(activity, list);
-        gameList.setLayoutManager(new LinearLayoutManager(getContext()));
+        List<String> list2 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list2.add("");
+        }
+
+        List<String> list3 = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            list3.add("");
+        }
+
+
+        List<List<String>> lists = new ArrayList<>();
+
+        lists.add(list1);
+        lists.add(list2);
+        lists.add(list3);
+
+        adapter = new GameListAdapter(activity, lists);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(activity);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        gameList.setLayoutManager(linearLayoutManager);
         gameList.setAdapter(adapter);
     }
 
@@ -73,4 +107,19 @@ public class CertainGamesFragment extends BaseMVPFragment<GamesPresenter> implem
         Log.d(TAG, "showGames");
     }
 
+    @OnClick({R.id.left_arrow, R.id.right_arrow})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.left_arrow:
+                if (currentPos > 0) {
+                    gameList.smoothScrollToPosition(--currentPos);
+                }
+                break;
+            case R.id.right_arrow:
+                if (currentPos < adapter.getItemCount() - 1) {
+                    gameList.smoothScrollToPosition(++currentPos);
+                }
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/alp/mvp/widgete/LazyRecycleView.java
@@ -0,0 +1,28 @@
+package com.alp.mvp.widgete;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class LazyRecycleView extends RecyclerView {
+
+    public LazyRecycleView(Context context) {
+        super(context);
+    }
+
+    public LazyRecycleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LazyRecycleView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        return false;
+    }
+    
+}
